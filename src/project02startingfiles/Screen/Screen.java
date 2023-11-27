fe11954dde04c17a8b7c1ebf0520608583645a71
s566011 <s566011@nwmissouri.edu>
2023-11-27 11:20:36 -0600

    Added Screen class methods and constructor

--- src/project02startingfiles/Screen/Screen.java
+++ src/project02startingfiles/Screen/Screen.java
@@ -1,3 +1,17 @@
 package project02startingfiles.Screen;
+import project02startingfiles.PlayerData.Player;
 public class Screen {
+    private Player playerData;
+    public Screen(Player playerData){
+        this.playerData = playerData;
+    }
+    public void status(){
+
+    }
+    public void movement(){
+
+    }
+    public void startCombat(){
+
+    }
 }

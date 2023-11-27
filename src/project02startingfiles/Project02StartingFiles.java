e5980d5e2b41a236eeeb27c867477e64636716e2
s566011 <s566011@nwmissouri.edu>
2023-11-27 11:40:01 -0600

    Added main stuff

--- src/project02startingfiles/Project02StartingFiles.java
+++ src/project02startingfiles/Project02StartingFiles.java
@@ -1,6 +1,7 @@
 package project02startingfiles;
 
 import project02startingfiles.PlayerData.*;
+import java.util.*;
 
 /**
  *
@@ -11,6 +12,17 @@ public class Project02StartingFiles {
     public static void main(String[] args) {
         Player test = new Knight(22, 22);
         System.out.println(test.getScore());
+
+        boolean quitFlag = false;
+        Scanner input = new Scanner(System.in);
+        while(quitFlag = false){
+            System.out.println("What would you like to do?");
+            System.out.println("“{?}Status Report || " +
+                    "{n} {s} {e} {w} Move 1 Space North, South, East, or West || " +
+                    "{q}Quit");
+
+            //3 if statements for status report, movement, and quit.
+        }
     }
 
 }
